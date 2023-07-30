94b7f27a4a556ab52e6207b389b4a277b28da6c7
Angelo Ricardo <angeloengcomp@gmail.com>
2023-07-30 15:44:46 -0300

    feat: implements login

---
+++ pages/api/auth/login.js
@@ -0,0 +1,32 @@
+import NextCors from 'nextjs-cors';
+import { v4 as uuidv4 } from 'uuid';
+export default async function handler(req, res) {
+
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  try {
+    if (password == '123456' && email == 'hermes') {
+      return res.status(200).json({
+        error: false,
+        message: 'Sem erros',
+        results: {
+          token: uuidv4(),
+        },
+      });
+    } else {
+      throw 'myException'; // gera uma exceção
+    }
+  } catch (error) {
+    res.status(401).json({
+      error: true,
+      message: 'Login ou senha incorretos',
+    });
+  }
+
+  res.status(200).json(data);
+}
