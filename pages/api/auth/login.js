2cbe9a2ab7794b0e6cc6cb373412476e2c6b0804
Angelo Ricardo <angeloengcomp@gmail.com>
2023-07-30 16:25:40 -0300

    hotfix: close login by method

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,7 +1,6 @@
 import NextCors from 'nextjs-cors';
 import { v4 as uuidv4 } from 'uuid';
 export default async function handler(req, res) {
-
   await NextCors(req, res, {
     // Options
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
@@ -9,24 +8,29 @@ export default async function handler(req, res) {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   });
 
-  try {
-    if (password == '123456' && email == 'hermes') {
-      return res.status(200).json({
-        error: false,
-        message: 'Sem erros',
-        results: {
-          token: uuidv4(),
-        },
+  if (req.method === 'POST') {
+    try {
+      if (password == '123456' && email == 'hermes') {
+        return res.status(200).json({
+          error: false,
+          message: 'Sem erros',
+          results: {
+            token: uuidv4(),
+          },
+        });
+      } else {
+        throw 'myException'; // gera uma exceção
+      }
+    } catch (error) {
+      res.status(401).json({
+        error: true,
+        message: 'Login ou senha incorretos',
       });
-    } else {
-      throw 'myException'; // gera uma exceção
     }
-  } catch (error) {
-    res.status(401).json({
-      error: true,
-      message: 'Login ou senha incorretos',
-    });
-  }
 
-  res.status(200).json(data);
+    res.status(200).json(data);
+  } else {
+    res.status(405).json({ message: 'Método não permitido' });
+    return;
+  }
 }
