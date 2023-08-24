02459f3376dc18518cbc594ddf40420f17e0c639
Angelo Ricardo <angeloengcomp@gmail.com>
2023-08-24 12:29:37 -0300

    feat: add favoritos/toogle

---
+++ pages/api/favoritos/toogle.js
@@ -0,0 +1,29 @@
+import NextCors from 'nextjs-cors';
+
+export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  if (!req.body.id) {
+    res.status(422).json({
+      error: true,
+      message: 'ID não informado',
+    });
+  }
+
+  if (req.method === 'POST') {
+    res.status(200).json({
+      error: false,
+      message: req.body.id === 1 ? 'Favoritado' : 'Desfavoritado',
+    });
+  } else {
+    res.status(405).json({
+      error: true,
+      message: 'Método não suportado',
+    });
+  }
+}
