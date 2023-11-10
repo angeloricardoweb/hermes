521b2158bade204580adc78789a58e9a0e23f9e1
Angelo Ricardo <angeloengcomp@gmail.com>
2023-11-10 16:21:17 -0300

    Refactor code to improve readability and performance.

---
+++ pages/api/auth/register.js
@@ -0,0 +1,25 @@
+import NextCors from 'nextjs-cors';
+import { v4 as uuidv4 } from 'uuid';
+export default async function handler(req, res) {
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
+  if (req.method === 'POST') {
+    return res.status(200).json({
+      error: false,
+      message: 'Sem erros',
+      payload: req.body,
+      results: {
+        token: uuidv4(),
+      },
+    });
+
+  } else {
+    res.status(405).json({ message: 'Método não permitido' });
+    return;
+  }
+}
