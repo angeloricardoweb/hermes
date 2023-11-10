58da3ee25167d5c7b78f773fc2fbd47599eb7fbd
Angelo Ricardo <angeloengcomp@gmail.com>
2023-11-10 17:43:47 -0300

    Update registration API response message.

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -11,8 +11,7 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     return res.status(200).json({
       error: false,
-      message: 'Sem erros',
-      payload: req.body,
+      message: 'Cadastro realizado com sucesso',
       results: {
         token: uuidv4(),
       },
