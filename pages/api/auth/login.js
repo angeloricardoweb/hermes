663a25370a97fbfd07010140de9441946d806905
Angelo Ricardo <angeloengcomp@gmail.com>
2023-11-11 01:06:42 -0300

    Update registration API response message.

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
   if (req.method === 'POST') {
     try {
-      if (req.body.password == '123456' && req.body.email == 'hermes') {
+      if (req.body.password == '123456' && req.body.email == 'teste@email.com') {
         return res.status(200).json({
           error: false,
           message: 'Sem erros',
