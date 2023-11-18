02de6e0d3e49bc12352c5143eec7970f6138a305
Angelo Ricardo <angeloengcomp@gmail.com>
2023-11-18 00:58:33 -0300

    Add role property to registration response

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
       message: 'Cadastro realizado com sucesso',
       results: {
         token: uuidv4(),
+        role: "baby"
       },
     });
 
